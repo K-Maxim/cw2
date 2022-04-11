de9276c59c0df147ff8f690c31a616ac21b550a8
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-04-11 19:22:39 +0300

    добавил ограничения в 10 постов
    добавил теги, переход по фотке и имени, а также добавление в закладки

--- search/view.py
+++ search/view.py
@@ -9,8 +9,12 @@ search_blueprint = Blueprint('search_blueprint', __name__, template_folder='temp
 def search():
     search_post = request.args['s']
     post_by_keyword = utils.search_for_posts(search_post)
-    quantity_contents = len(post_by_keyword)
+    quantity_contents = len(post_by_keyword[:10])
+    comments = utils.get_comments_all()
+    post_by_tag, tags = utils.get_posts_by_tag()
+    comments_count = utils.get_comments_count(comments)
     return render_template('search.html', search_post=search_post,
                            quantity_contents=quantity_contents,
-                           post_by_keyword=post_by_keyword)
+                           post_by_keyword=post_by_keyword[:10],
+                           comments_count=comments_count, tags=tags)
 
